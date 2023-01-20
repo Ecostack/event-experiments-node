0668ccca738c27cbf59c67fec92bbbb05da3ce45
Sebastian Scheibe <sebastian@ecostack.nl>
2023-01-20 16:54:02 +0800

    add prefetch count

--- src/consumer.ts
+++ src/consumer.ts
@@ -5,13 +5,15 @@ import {exchange, queue, routing_key} from "./constants";
     const conn = await amqplib.connect('amqp://localhost');
 
     const ch = await conn.createChannel();
+    // maximum messages to take before reaching no-ack for this consumer
+    await ch.prefetch(2)
     await ch.assertExchange(exchange, "direct", {durable:true})
     await ch.assertQueue(queue, {durable:true})
     await ch.bindQueue(queue,exchange,routing_key)
     await ch.consume(queue,(msg)=>{
 
         console.log('Received:', msg.content.toString());
-        setTimeout(() => ch.ack(msg),200)
-    })
+        setTimeout(() => ch.ack(msg),1000)
+    },{noAck:false})
 
 })();
